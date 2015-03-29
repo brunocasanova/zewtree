7800630e192dd70241240b5ccf480bcf617e5ed3
brunocasanova <deftonicks_me@hotmail.com>
2015-03-29 17:46:53 +0100

    Testing_4...

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ module.exports = (function (){
 	var HOST = process.env.HOST ||  process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 	var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3080;
 
-	console.log( 'server initialized..' );
-
+	console.log( 'PORT' + process.env.OPENSHIFT_NODEJS_PORT );
+	console.log( 'IP' + process.env.OPENSHIFT_NODEJS_IP );
+	
 	var server = http.createServer(function ( req, res ){
 		console.log( 'request' );
 		res.end( 'Server running at: ' + PROTOCOL + '://' + HOST + ': ' + PORT + ' - worker id: ' + process.pid );
 	})
-	.listen( PORT, function(){
+	.listen( PORT, function (){
 		console.log( 'Server running at: ' + PROTOCOL + '://' + HOST + ': ' + PORT + ' - worker id: ' + process.pid );
 	});
 
