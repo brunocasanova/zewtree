af549f2e39801bb3c9b56d537e5b9667b53e3686
brunocasanova <deftonicks_me@hotmail.com>
2015-03-29 19:10:32 +0100

    Testing...

--- server.js
+++ server.js
@@ -3,109 +3,103 @@
 var express = require( 'express' );
 var fs = require('fs');
 
-var SampleApp = function() {
+var server = function() {
 
-/**
- *  DEFINE
- */
+	//DEFINE
 
-    var self = this;
+	var self = this;
 
-    self.setupVariables = function() {
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+	self.setupVariables = function() {
+		self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+		self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-        if ( typeof self.ipaddress === 'undefined' ) {
-            console.warn( 'No OPENSHIFT_NODEJS_IP var, using 127.0.0.1' );
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+		if ( typeof self.ipaddress === 'undefined' ) {
+			console.warn( 'No OPENSHIFT_NODEJS_IP var, using 127.0.0.1' );
+			self.ipaddress = "127.0.0.1";
+		};
 
-    self.populateCache = function() {
-        if ( typeof self.zcache === 'undefined' ) {
-            self.zcache = { 'index.html': '' };
-        }
+	};
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+	//SERVER
 
-    self.cache_get = function( key ) { return self.zcache[key]; };
+	self.initialize = function() {
+		self.setupVariables();
+		self.populateCache();
+		self.setupTerminationHandlers();
 
-    self.terminator = function( sig ){
-        if ( typeof sig === 'string' ){
-           console.log('%s: Received %s - terminating sample app ...', Date( Date.now() ), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date( Date.now() ) );
-    };
+		// Create the express server and routes.
+		self.initializeServer();
+	};
 
-    self.setupTerminationHandlers = function(){
-        process.on('exit', function() { self.terminator(); });
+	self.initializeServer = function() {
+		self.createRoutes();
+		self.app = express();
 
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function ( element, index, array ){
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
+		//  Add handlers for the app (from the routes).
+		for( var r in self.routes) {
+			self.app.get( r, self.routes[r] );
+		}
+	};
 
-/**
- *  APP
- */
+	self.start = function() {
+		self.app.listen(self.port, self.ipaddress, function() {
+			console.log('%s: Node server started on %s:%d ...', Date(Date.now() ), self.ipaddress, self.port );
+		});
+	};
 
-    self.createRoutes = function() {
-        self.routes = { };	
+	//ROUTES
+	
+	self.createRoutes = function() {
+		self.routes = {};	
 
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
+		self.routes['/'] = function ( req, res ){
 
-        self.routes['/'] = function ( req, res ){
+			console.log( 'request: ' + request.connection.remoteAddress );
+			res.setHeader( 'Content-Type', 'text/html' );
 
-        	console.log( 'request: ' + req );
-            res.setHeader('Content-Type', 'text/html');
-            res.send( self.cache_get('index.html') );
+			res.send( self.cache_get('index.html') );
 
-        };
-    };
+		};
+	};
 
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express();
+	//CACHE
 
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get( r, self.routes[r] );
-        }
-    };
+	self.populateCache = function() {
+		if ( typeof self.zcache === 'undefined' ) {
+			self.zcache = { 'index.html': '' };
+		}
 
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
+		//  Local cache for static content.
+		self.zcache['index.html'] = fs.readFileSync('./index.html');
+	};
 
-        // Create the express server and routes.
-        self.initializeServer();
-    };
+	self.cache_get = function( key ) { return self.zcache[key]; };
 
-    self.start = function() {
+	//TERMINATE
 
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...', Date(Date.now() ), self.ipaddress, self.port );
-        });
-    };
+	self.terminator = function( sig ){
+		if ( typeof sig === 'string' ){
+		   console.log('%s: Received %s - terminating zewtree ...', Date( Date.now() ), sig );
+		   process.exit( 1 );
+		}
+		console.log( '%s: Node server stopped.', Date( Date.now() ) );
+	};
+
+	self.setupTerminationHandlers = function(){
+		process.on('exit', function() { self.terminator(); });
+
+		['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+		 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+		].forEach(function ( element, index, array ){
+			process.on( element, function() { self.terminator( element ); });
+		});
+	};
 
 };
 
 
-/**
- *  MAIN
- */
+var app = new server();
 
-var zapp = new SampleApp();
-
-zapp.initialize();
-zapp.start();
+app.initialize();
+app.start();
 
